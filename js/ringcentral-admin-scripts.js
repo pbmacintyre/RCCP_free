07b93d7f7b553a29925aa77c8cfcfd1ad0ee3476
pbmacintyre <pbmacintyre@gmail.com>
2019-04-29 01:22:26 -0300

    Added Google ReCaptcha to the 2 widgets and all aspects of it
    Added Google ReCaptcha to 2 widget forms, added it to the admin settings page, add the 2 elements to JavaScript code - show / hide passwords, added DB table columns for it, added save code and edits to test for required fields, created functions for building the recpatcha.

--- js/ringcentral-admin-scripts.js
+++ js/ringcentral-admin-scripts.js
@@ -4,23 +4,33 @@
  */ 
 
 function reveal_it() {
-	var x = document.getElementById("myClientSecret");
-    var y = document.getElementById("myRCPassword");    
-	if (x.type === "password") {
-	  x.type = "text";
+	var w = document.getElementById("myClientSecret");
+	var x = document.getElementById("myRCPassword");    
+    var y = document.getElementById("myGRCSite");    
+	
+    if (w.type === "password") {
+	  w.type = "text";
 	} 
+//	if (x.type === "password") {
+//	  x.type = "text";
+//	}
 	if (y.type === "password") {
-		y.type = "text";
-	} 
+	  y.type = "text";
+	}
 }
 
 function hide_it() {
-	var x = document.getElementById("myClientSecret");
-    var y = document.getElementById("myRCPassword");    
-	if (x.type === "text") {	  
-	  x.type = "password";
+	var w = document.getElementById("myClientSecret");
+	var x = document.getElementById("myRCPassword");    
+    var y = document.getElementById("myGRCSite");    
+	
+    if (w.type === "text") {	  
+	  w.type = "password";
 	}
+//	if (x.type === "text") {
+//	  x.type = "password";
+//	}
 	if (y.type === "text") {
-	    y.type = "password";
+	  y.type = "password";
 	}
 }
--- js/ringcentral-scripts.js
+++ js/ringcentral-scripts.js
@@ -11,23 +11,41 @@ function isNumberKey(evt){
 }
 
 function reveal_it() {
-	var x = document.getElementById("myClientSecret");
-    var y = document.getElementById("myRCPassword");    
+	var w = document.getElementById("myClientSecret");
+	var x = document.getElementById("myRCPassword");    
+	var y = document.getElementById("myGRCSite");    
+    var z = document.getElementById("myGRCSecret");    
+	
+    if (w.type === "password") {
+	  w.type = "text";
+	} 
 	if (x.type === "password") {
 	  x.type = "text";
-	} 
+	}
 	if (y.type === "password") {
-		y.type = "text";
-	} 
+	  y.type = "text";
+	}
+	if (z.type === "password") {
+	  z.type = "text";
+	}
 }
 
 function hide_it() {
-	var x = document.getElementById("myClientSecret");
-    var y = document.getElementById("myRCPassword");    
-	if (x.type === "text") {	  
+	var w = document.getElementById("myClientSecret");
+	var x = document.getElementById("myRCPassword");    
+    var y = document.getElementById("myGRCSite");  
+    var z = document.getElementById("myGRCSecret");
+	
+    if (w.type === "text") {	  
+	  w.type = "password";
+	}
+	if (x.type === "text") {
 	  x.type = "password";
 	}
 	if (y.type === "text") {
-	    y.type = "password";
+	  y.type = "password";
+	}
+	if (z.type === "text") {
+	  z.type = "password";
 	}
 }
